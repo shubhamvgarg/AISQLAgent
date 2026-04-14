0a306c6ff9760a34dd6d21c9047c4e4e38f79b20
Shubham Garg <shubham07119garg@gmail.com>
2026-04-14 17:45:54 +0530

    created frontend using streamlit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from graph.graph_builder import build_graph
+
+st.set_page_config(page_title="AskSQL", layout="wide")
+
+st.title("💬 AskSQL")
+st.write("Ask questions in plain English and get SQL results.")
+
+query = st.text_input("Enter your question:")
+
+if st.button("Run Query"):
+    if query:
+        graph = build_graph()
+
+        with st.spinner("Thinking..."):
+            response = graph.invoke({
+                "question": query
+            })
+
+        if response.get("error"):
+            st.error(response["error"])
+        else:
+            st.subheader("Generated SQL")
+            st.code(response["validated_sql"], language="sql")
+
+            st.subheader("Results")
+            st.write(response["result"])
